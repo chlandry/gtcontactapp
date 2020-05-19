0afac78e8d6eb46bfba8238961c6eec3c44471de
Chris Landry <lordhatsuma@gmail.com>
2020-05-19 19:58:43 -0400

    initial commit

---
+++ ContactWebApp/Data/PersonContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ContactWebApp.Models;
+
+namespace ContactWebApp.Data
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext (DbContextOptions<PersonContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContactWebApp.Models.Person> Person { get; set; }
+    }
+}
---
+++ ContactWebApp/Models/Person.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactWebApp.Models
+{
+    public class Person
+    {
+        [Display(Name = "ID")]
+        public int id { get; set; }
+        [Display(Name = "First")]
+        public String firstName { get; set; }
+        [Display(Name = "Last")]
+        public String lastName { get; set; }
+        [Display(Name = "Email")]
+        public String emailAddresses { get; set; }
+        [Display(Name = "Phone")]
+        public String phoneNumbers { get; set; }
+        [Display(Name = "Birthday")]
+        [DataType(DataType.Date)] //date only, no time       
+        public DateTime birthDay { get; set; }
+
+        //calculated by birthday, readonly
+        public long Age 
+        { 
+            get
+            {
+                return DateAndTime.DateDiff(DateInterval.Year, birthDay, DateTime.Now);
+            } 
+        }
+
+    }
+}
